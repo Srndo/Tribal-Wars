498461a622af68d0c443cd519928aee4f8fee19f
Srndo <xsesta06@stud.fit.vutbr.cz>
2020-03-16 16:59:16 +0100

    Update premiumMarketBuy.js

--- Skripty/premiumMarketBuy.js
+++ Skripty/premiumMarketBuy.js
@@ -1,11 +1,13 @@
-var sound = new Audio("https://freesound.org/data/previews/57/57806_533680-lq.mp3");
+var wood_sound = new Audio("https://freesound.org/data/previews/178/178740_2888453-lq.mp3");
+var clay_sound = new Audio("https://freesound.org/data/previews/145/145504_2580450-lq.mp3");
+var iron_sound = new Audio("https://freesound.org/data/previews/371/371922_2402876-lq.mp3");
 var wood = document.getElementById("premium_exchange_stock_wood").textContent;
 var clay = document.getElementById("premium_exchange_stock_stone").textContent;
 var iron = document.getElementById("premium_exchange_stock_iron").textContent;
 var not_printed = true;
 
 // Function for com
-function compare(stock, value, not_printed, selector) {
+function compare(stock, value, not_printed, selector, sound) {
 	if (stock >= value && not_printed)
 	{
 		not_printed = false;
@@ -28,9 +30,9 @@ function check(){
 	clay = document.getElementById("premium_exchange_stock_stone").textContent;
 	iron = document.getElementById("premium_exchange_stock_iron").textContent;
 
-	not_printed = compare(wood, 400, not_printed, 9);
-	not_printed = compare(clay, 400, not_printed, 12);
-	not_printed = compare(iron, 400, not_printed, 15);
+	not_printed = compare(wood, 500, not_printed, 9, wood_sound);
+	not_printed = compare(clay, 500, not_printed, 12, clay_sound);
+	not_printed = compare(iron, 500, not_printed, 15, iron_sound);
 
 	var fader = document.getElementById("fader");
 	
